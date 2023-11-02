a2ad1b8b7fca11ee46164e578b413f69d5e66313
Thiago Rocha <mrthiago09@gmail.com>
2023-11-02 10:44:25 -0300

    My first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function toggleMode() {
+  const html = document.documentElement;
+  html.classList.toggle("light");
+
+  var img = document.querySelector("#profile img");
+
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src","./assets/avatar-light.png");
+    img.setAttribute(
+      "akt",
+      "foto de thiago rocha utilizando camisa preta, sem oculos  e fundo de um quarto."
+    )
+  } else {
+    html.classList.remove("light");
+    img.setAttribute("src", "./assets/avatar.png");
+    img.setAttribute("alt", "foto de thiago rocha utilizando camisa preta, oculos  e fundo cinza.")
+  }
+}
+
+
